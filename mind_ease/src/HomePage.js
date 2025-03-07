8a9fbcae57af5a964fdf4927c4eacb175db2f3be
Shamik <119611241+03shamik@users.noreply.github.com>
2025-03-08 01:14:35 +0530

    cerated the homepage and made the routing

---
+++ mind_ease/src/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HomePage.css";
+
+const HomePage = () => {
+    return (
+        <div className="homepage-container">
+            <h1>Welcome to MindEase</h1>
+            <p>Your journey to mindfulness begins here.</p>
+        </div>
+    );
+};
+
+export default HomePage;
+ 
